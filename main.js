f3f67ca0494e3a516f877d7068b5e6d645024999
EC2 Default User <ec2-user@ip-172-31-77-252.ec2.internal>
2022-02-06 06:16:55 +0000

    syuusei

---
+++ main.js
@@ -0,0 +1,38 @@
+var POTEPANCAMP = POTEPANCAMP || {};
+POTEPANCAMP.HAMBURGER_MENU = {
+  init: function () {
+    if (!this.setParams()) return;
+    this.prepare();
+    this.bindEvents();
+  },
+  setParams: function () {
+    this.$hamburgerMenu = $('.jsc-hamburger');
+    this.$hamburgerList = $('.jsc-hamburger-list');
+    this.$siteHeader = $('.site-header');
+    this.siteHeaderHeight = this.$siteHeader.outerHeight();
+    this.hamburgerListHeight = this.$hamburgerList.outerHeight();
+    this.scrollMenu();
+    if (this.$hamburgerMenu.length == 0) return false;
+    return true;
+  },
+  prepare: function () {},
+  bindEvents: function () {
+    this.$hamburgerMenu.on('click', $.proxy(this.toggleMenu, this));
+  },
+  toggleMenu: function () {
+    this.$hamburgerMenu.toggleClass('is-active');
+    this.$hamburgerList.toggleClass('is-active');
+    var self = this;
+    $(window).resize(function () {
+      self.scrollMenu();
+    });
+  },
+  scrollMenu: function () {
+    var self = this;
+    if (window.innerHeight < self.siteHeaderHeight + self.hamburgerListHeight) {
+      self.$hamburgerList.addClass('is-scroll');
+    } else {
+      self.$hamburgerList.removeClass('is-scroll');
+    }
+  }
+};
